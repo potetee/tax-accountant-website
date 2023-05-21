307edab990122a93f4aaf261a68cdaa1a5e86eeb
Harry Potter <ljfeihtdw@gmail.com>
2023-05-21 19:40:08 +0900

    before add the redux tool kit

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -3,11 +3,12 @@ import { Route,Routes } from "react-router-dom";
 import About from "./component/About/About";
 import Service from "./component/Services/Services";
 import Contact from "./component/Contact/Contact";
+import Home from "./component/Home/Home";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<About />} />
+      <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       <Route path="/services" element={<Service />} />
       <Route path="/contact" element={<Contact />} />
