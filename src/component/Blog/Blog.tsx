a4e6f1990c8f61162fe554bcc615f8521cce14c7
Harry Potter <ljfeihtdw@gmail.com>
2023-06-19 17:16:40 +0900

    finished the pc version

--- src/component/Blog/Blog.tsx
+++ src/component/Blog/Blog.tsx
@@ -1,9 +1,39 @@
 import React from "react";
 import styles from "./Blog.module.css";
+import {blogData} from "./BlogContent";
 
 const Blog: React.FC = () =>{
     return(
-        <div className={styles.color}>Blog</div>
+        <>
+            <div className={styles.blogTitle}>
+                <div className={styles.blogTitleFirst}>OUR </div>
+                <div>BLOG</div>
+            </div>
+            <div className={styles.blogBase}>
+                <div className={styles.blogContentBase}>
+                    {
+                        blogData.map((item,index)=>{
+                            return(
+                                <div className={styles.blogContent} key={index}>
+                                    {/*<div className={`${styles.blogContentTitle} ${index % 2 === 0 ? styles.blogContentLeft : styles.blogContentRight}`}>*/}
+                                    <div  className={`${index % 2 === 0 ? styles.blogContentLeft : styles.blogContentRight}`}>
+                                        <div className={styles.blogContentTitle}>
+                                            <div className={styles.blogContentDate}>{item.date}</div>
+                                            <div>  </div>
+                                            <div className={styles.blogContentTitle}>{item.title}</div>
+                                        </div>
+                                        <div className={styles.blogContentContent}>{item.content}</div>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    })
+
+                </div>
+            </div>
+
+        </>
+
     )
 }
 
---
+++ src/component/Blog/BlogContent.ts
@@ -0,0 +1,28 @@
+interface BlogData {
+    title: string;
+    content: string;
+    date: string;
+}
+
+export const blogData: BlogData[] = [
+    {
+        title: "Blog 1",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl vitae aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nisl vitae nisl. Donec euismod, nisl vitae aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nisl vitae nisl.",
+        date: "2021-01-01"
+    },
+    {
+        title: "Blog 2",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl vitae aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nisl vitae nisl. Donec euismod, nisl vitae aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nisl vitae nisl.",
+        date: "2021-01-02"
+    },
+    {
+        title: "Blog 3",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl vitae aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nisl vitae nisl. Donec euismod, nisl vitae aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nisl vitae nisl.",
+        date: "2021-01-03"
+    },
+    {
+        title: "Blog 4",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl vitae aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nisl vitae nisl. Donec euismod, nisl vitae aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nisl vitae nisl.",
+        date: "2021-01-04"
+    }
+];
