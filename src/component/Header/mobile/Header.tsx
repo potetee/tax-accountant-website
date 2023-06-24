e5ecb611ec1fa08d4270a81c56a0fc5197378b86
Harry Potter <ljfeihtdw@gmail.com>
2023-06-24 20:43:20 +0900

    before making the home section

--- src/component/Header/mobile/Header.tsx
+++ src/component/Header/mobile/Header.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import HomeTwoToneIcon from '@material-ui/icons/HomeTwoTone';
 import MenuSharpIcon from '@material-ui/icons/MenuSharp';
 import styles from './Header.module.css';
+import {Link} from "react-router-dom";
 
 const MobileComponent:React.FC<MobileComponentProps> = ({setSidebar})=>{
 
     return (
         <>
             <div className={styles.headerBack}>
-                <HomeTwoToneIcon  fontSize={'inherit'}/>
+                <Link className={styles.link} to='/'><HomeTwoToneIcon  fontSize={'inherit'}/></Link>
                 <MenuSharpIcon  onClick={setSidebar}  fontSize={'inherit'}/>
             </div>
         </>
