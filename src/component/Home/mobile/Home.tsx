e8d85b16b2ccd975865d2108fb78c0cdc047680c
Harry Potter <ljfeihtdw@gmail.com>
2023-06-25 09:33:58 +0900

    before  change  mobile about page

--- src/component/Home/mobile/Home.tsx
+++ src/component/Home/mobile/Home.tsx
@@ -1,10 +1,19 @@
 import React from "react";
+import styles from "./Home.module.css";
 
 const Home: React.FC = () =>{
     return(
-        <div>
-            <h1>Mobile Home</h1>
-        </div>
+      <>
+          <div className={styles.homeContent}>
+              <div className={styles.homeContentBackground}>
+                  <div className={styles.homeContentTitle}>
+                      <div>ACCOUNTING & </div>
+                      <div className={styles.homeContentTitleSecond}>FINANCIAL SERVICES</div>
+                  </div>
+                  <div className={styles.homeContentDescription}>Your financial future matters - Let us help you navigate the path to success. </div>
+              </div>
+          </div>
+      </>
     );
 }
 
