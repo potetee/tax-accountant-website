b20748bed5b1e44290fd2a5ed4db2582868c7296
Harry Potter <ljfeihtdw@gmail.com>
2023-07-09 09:40:17 +0900

    change the url

--- src/features/data/api.ts
+++ src/features/data/api.ts
@@ -2,7 +2,7 @@ import {createAsyncThunk} from "@reduxjs/toolkit";
 import axios from "axios";
 
 export const fetchData = createAsyncThunk('data/fetchData',async () => {
-    const response = await axios.get('http://localhost:8080/api/test4');
+    const response = await axios.get('http://localhost:8080/api/inquery');
     return response.data;
 });
 
