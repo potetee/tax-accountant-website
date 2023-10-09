e9ff32c60434387ece14357d22a228c37db14e82
Yasuyuki Ueda <>
2023-10-09 15:05:05 +0900

    change the about and service

--- src/component/Services/mobile/Servcies.tsx
+++ src/component/Services/mobile/Servcies.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 import styles from "./Services.module.css";
+import {
+    SERVICE1,
+    SERVICE1_DESCRIPTION,
+    SERVICE1_LIST,
+    SERVICE2,
+    SERVICE2_DESCRIPTION,
+    SERVICE2_LIST
+} from "../../../utils/Messages";
 
 
 const Services:React.FC = () =>{
@@ -13,39 +21,40 @@ const Services:React.FC = () =>{
                 <div>
                     <div className={styles.eachServiceOutline}>
                         <div>
-                            <div className={styles.serviceEachTitle}>開業サポート</div>
-                            <div className={styles.serviceBorder}/>
-                        </div>
-                        <div>
-                            <div className={styles.centerImage}>
-                                <img src={process.env.PUBLIC_URL + '/openup.jpg'} alt="dummy" className={styles.eachPicture}/>
-                            </div>
-                            <div>あなたのビジネスをスムーズにスタートさせるための包括的なサポートを提供します。これにはビジネスプランの作成、設立手続きの支援、財務管理の助言などが含まれます。</div>
-                        </div>
-
-                    </div>
-                    <div className={styles.eachServiceOutline}>
-                        <div>
-                            <div className={styles.serviceEachTitle}>税務顧問</div>
+                            <div className={styles.serviceEachTitle}>{SERVICE1}</div>
                             <div className={styles.serviceBorder}/>
                         </div>
                         <div>
                             <div className={styles.centerImage}>
                                 <img src={process.env.PUBLIC_URL + '/taxAdvice.jpg'} alt="dummy" className={styles.eachPicture}/>
                             </div>
-                            <div>法規制を遵守しつつ、最適な税務戦略を形成するために必要なアドバイスとガイダンスを提供します。これには、税務申告の準備や税務監査のサポートなどが含まれます。</div>
+                            <div>{SERVICE1_DESCRIPTION}</div>
+                            <div className={styles.serviceList}>
+                                {SERVICE1_LIST.map((item, index) => {
+                                    return <li key={index}>
+                                        {item}
+                                    </li>
+                                })}
+                            </div>
                         </div>
                     </div>
                     <div className={styles.eachServiceOutline}>
                         <div>
-                            <div className={styles.serviceEachTitle}>相続対策</div>
+                            <div className={styles.serviceEachTitle}>{SERVICE2}</div>
                             <div className={styles.serviceBorder}/>
                         </div>
                         <div>
                             <div className={styles.centerImage}>
                                 <img src={process.env.PUBLIC_URL + '/inheritance.jpg'} alt="dummy" className={styles.eachPicture}/>
                             </div>
-                            <div>遺産分割、遺贈税、その他の相続問題に関する専門的なアドバイスを提供します。あなたとあなたの家族が将来を安心して計画できるよう支援します。</div>
+                            <div>{SERVICE2_DESCRIPTION}</div>
+                            <div className={styles.serviceList}>
+                                {SERVICE2_LIST.map((item, index) => {
+                                    return <li key={index}>
+                                        {item}
+                                    </li>
+                                })}
+                            </div>
                         </div>
                     </div>
                 </div>
